2d060dfbd5b089228370c05a18dc7c3142b8b24e
orvice <orvice@gmail.com>
2017-02-19 21:48:48 +0800

    init ssr

--- src/ShadowsocksR.php
+++ src/ShadowsocksR.php
@@ -6,5 +6,21 @@ namespace Shadowsocks;
 
 class ShadowsocksR
 {
+    const protocol = [
+        // origin,verify_sha1,auth_sha1_v2,auth_sha1_v4,auth_aes128_md5,auth_aes128_sha1
+        "origin",
+        "verify_sha1",
+        "auth_sha1_v2",
+        "auth_sha1_v4",
+        "auth_aes128_md5",
+        "auth_aes128_sha1"
+    ];
 
+    const  plugin = [
+        //  plain,http_simple,http_post,tls1.2_ticket_auth
+        "plain",
+        "http_simple_compatible",
+        "http_post_compatible",
+        "tls1.2_ticket_auth_compatible"
+    ];
 }
