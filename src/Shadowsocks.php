0b70e6237e8e800e47bf00f7ad225f292d2640f1
orvice <orvice@gmail.com>
2017-02-19 21:46:44 +0800

    update clipher

--- src/Shadowsocks.php
+++ src/Shadowsocks.php
@@ -8,7 +8,17 @@ class Shadowsocks
 {
     const Cipher = [
         "rc4-md5",
+        "rc4-md5-6",
+        "aes-128-ctr",
+        "aes-192-ctr",
+        "aes-256-ctr",
+        "aes-128-cfb",
+        "aes-192-cfb",
         "aes-256-cfb",
+        "chacha20-ietf",
+
+        "chacha20",
+        "salsa20",
     ];
 
 }
